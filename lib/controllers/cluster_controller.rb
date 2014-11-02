4e59ed8b90c16fcc60be217860a53c233ba8a4a9
Julien Pellet <pelletj@gmail.com>
2014-11-02 08:45:37 +0100

    rename AwsController in Haas::Aws

--- lib/controllers/cluster_controller.rb
+++ lib/controllers/cluster_controller.rb
@@ -3,9 +3,9 @@ class ClusterController
   def self.launch
     count = 2
     cluster=Haas::Cluster.create
-    if AwsController.nb_instance_available >= count
-      AwsController.create_key_pair
-      AwsController.launch_instances(cluster, 'us-west-2',count,'m3.medium')
+    if Haas::Aws.nb_instance_available >= count
+      Haas::Aws.create_key_pair
+      Haas::Aws.launch_instances(cluster, 'us-west-2',count,'m3.medium')
     else
       puts I18n.t('haas.not_enough_instances_available')
     end
