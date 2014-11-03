6e58e8c76ac0132d407459ce6b336240850f7a59
Julien Pellet <pelletj@gmail.com>
2014-11-03 20:26:14 +0100

    add helpers to get chef server and ambari helper

--- lib/models/cluster.rb
+++ lib/models/cluster.rb
@@ -7,5 +7,13 @@ class Haas
       random_str = (0...8).map { (65 + rand(26)).chr }.join
       self.name = "HAAS-#{random_str}"
     end
+
+    def get_chef_server
+      self.nodes.where('nodes.chef_server=?',true)
+    end
+
+    def get_ambari_server
+      self.nodes.where('nodes.ambari_server=?',true)
+    end
   end
 end
